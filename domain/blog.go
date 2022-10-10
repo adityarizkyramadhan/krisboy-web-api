dca9b549aef3be7f0d416b0e94efede60da2c11a
Muh Zaqi <zaqiikhwan22@gmail.com>
2022-10-10 21:22:14 +0700

    ```add pt 2``` json specification in domain struct

--- domain/blog.go
+++ domain/blog.go
@@ -38,60 +38,60 @@ type Blog struct {
 	MemoryPlatform          string `gorm:"varchar(255)" json:"memoryPlatform"`
 	StoragePlatform         string `gorm:"varchar(255)" json:"storagePlatform"`
 	OSPlatform              string `gorm:"varchar(255)" json:"osPlatform"`
-	DimensionsDesign        string
-	WeightDesign            string
-	DesignFeatures          string
-	ColorDesign             string
-	BatteryDesign           string
-	KeyboardDesign          string
-	ScreenTypeDisplay       string
-	SizeResolutionDisplay   string
-	TouchScreen             string
+	DimensionsDesign        string `gorm:"varchar(255)" json:"dimensionDesign"`
+	WeightDesign            string `gorm:"varchar(255)" json:"weightDesign"`
+	DesignFeatures          string `gorm:"varchar(255)" json:"designFeatures"`
+	ColorDesign             string `gorm:"varchar(255)" json:"colorDesign"`
+	BatteryDesign           string `gorm:"varchar(255)" json:"batteryDesign"`
+	KeyboardDesign          string `gorm:"varchar(255)" json:"keyboardDesign"`
+	ScreenTypeDisplay       string `gorm:"varchar(255)" json:"screenTypeDisplay"`
+	SizeResolutionDisplay   string `gorm:"varchar(255)" json:"sizeResolutionDisplay"`
+	TouchScreen             string `gorm:"varchar(255)" json:"touchScreen"`
 	//Mutivalue
-	Features               string
-	WifiConnectivity       string
-	BluetoothConnectivity  string
+	Features               string `gorm:"varchar(255)" json:"features"`
+	WifiConnectivity       string `gorm:"varchar(255)" json:"wifiConnectivity"`
+	BluetoothConnectivity  string `gorm:"varchar(255)" json:"bluetoothConnectivity"`
 	//Mutivalue
-	USBConnectivity  string
-	HDMIConnectivity string
+	USBConnectivity  string `gorm:"varchar(255)" json:"usbConnectivity"`
+	HDMIConnectivity string `gorm:"varchar(255)" json:"hdmiConnectivity"`
 	//Mutivalue
-	OtherConnectivity      string
-	SecurityFeature        string
-	MicrosoftOfficeFeature string
+	OtherConnectivity      string `gorm:"varchar(255)" json:"otherConnectivity"`
+	SecurityFeature        string `gorm:"varchar(255)" json:"securityFeature"`
+	MicrosoftOfficeFeature string `gorm:"varchar(255)" json:"microsoftOfficeFeature"`
 	//Mutivalue
-	OtherFeature string
+	OtherFeature string `gorm:"varchar(255)" json:"otherFeature"`
 	//Mutivalue
-	Weakness string
+	Weakness string `gorm:"varchar(255)" json:"weakness"`
 	//Mutivalue
-	Strengh string
+	Strengh string `gorm:"varchar(255)" json:"strength"`
 	//One to One
-	Rating Rating  `gorm:"foreignkey:BlogId"`
-	Tags   []Tag   `gorm:"foreignkey:BlogId"`
-	Photos []Photo `gorm:"foreignkey:BlogId"`
+	Rating Rating  `gorm:"foreignkey:BlogId" json:"rating"`
+	Tags   []Tag   `gorm:"foreignkey:BlogId" json:"tags"`
+	Photos []Photo `gorm:"foreignkey:BlogId" json:"photos"`
 }
 
 type Rating struct {
 	gorm.Model
-	BlogId      uint
-	Design      float32
-	Performance float32
-	Screen      float32
-	Batery      float32
-	Conectivity float32
-	Storage     float32
-	Average     float32
+	BlogId       uint `json:"blogId"`
+	Design       float32 `json:"design"`
+	Performance  float32 `json:"perfomance"`
+	Screen       float32 `json:"screen"`
+	Battery      float32 `json:"battery"`
+	Connectivity float32 `json:"connectivity"`
+	Storage      float32 `json:"storage"`
+	Average      float32 `json:"average"`
 }
 
 //Entitas Tag
 type Tag struct {
 	gorm.Model
-	BlogId uint
-	Type   int16
+	BlogId uint `json:"blogId"`
+	Type   int16 `json:"type"`
 }
 
 //Entitas Gambar
 type Photo struct {
 	gorm.Model
-	BlogId     uint
-	LinkPhotos uint
+	BlogId     uint `json:"blogId"`
+	LinkPhotos uint `json:"linkPhotos"`
 }
